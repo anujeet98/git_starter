3b0fb5a848e41d9c7a57c4feecf4b781c91e55ec
Anujeet Swain <anujeet98@gmail.com>
2023-09-12 04:09:22 +0530

    DOM task 4: querryselector

--- main.js
+++ main.js
@@ -13,18 +13,34 @@ pageHead.style.color = 'green';
 
 var items = document.getElementsByClassName('list-group-item');
 
-// for(var i=0; i<items.length;i++){
-//     items[i].style.fontWeight = 'bold';
-// }
+for(var i=0; i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
 
-items[2].style.backgroundColor = 'green';
-
-// console.log(items[2].style);
+// items[2].style.backgroundColor = 'green';
 
 
 var listTag = document.getElementsByTagName('li');
-console.log(listTag);
+// console.log(listTag);
 
-for(var i=0; i<listTag.length;i++){
-    listTag[i].style.fontWeight = 'bold';
+// for(var i=0; i<listTag.length;i++){
+//     listTag[i].style.fontWeight = 'bold';
+// }
+
+
+var itemSelect = document.querySelector('.list-group-item:nth-child(2)');
+
+itemSelect.style.backgroundColor = 'lightgreen';
+
+var itemHidden = document.querySelector('.list-group-item:nth-child(3)');
+itemHidden.style.visibility = 'hidden';
+
+
+var itemHighlight = document.querySelectorAll('.list-group-item');
+itemHighlight[1].style.color = 'green';
+
+var oddHighlight = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0; i<oddHighlight.length;i++){
+    oddHighlight[i].style.backgroundColor = 'green';
 }
