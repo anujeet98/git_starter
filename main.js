02b13d9f4fc28bf3f5638428175e880aab4d7c31
Anujeet Swain <anujeet98@gmail.com>
2023-09-12 03:30:33 +0530

    DOM task 2: highlight item  3 and make every items of list as bold

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ var pageHead = document.getElementById('page-header');
 
 pageHead.style.fontWeight = 'bold';
 pageHead.style.color = 'green';
+
+
+var items = document.getElementsByClassName('list-group-item');
+
+for(var i=0; i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
+
+items[2].style.backgroundColor = 'green';
+
+// console.log(items[2].style);
+
+console.log(items);
