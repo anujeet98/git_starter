55b75db26ea5ca0335820d0e404cc6b608168f07
Anujeet Swain <anujeet98@gmail.com>
2023-09-12 03:12:46 +0530

    DOM task 1 update: header made bold and green

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+var titleVar = document.getElementById('main-header');
+
+titleVar.style.borderBottom = 'solid 3px black';
+
+
+var pageHead = document.getElementById('page-header');
+
+pageHead.style.fontWeight = 'bold';
+pageHead.style.color = 'green';
