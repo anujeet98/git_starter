9d5a7ce084701da9a5dee0d8054410a8cc3eeaa7
Anujeet Swain <anujeet98@gmail.com>
2023-09-18 17:07:53 +0530

    use json stingify to store objects into localstorage with multiple users

--- bookingApp.js
+++ bookingApp.js
@@ -13,7 +13,11 @@ function submitMethod(e){
     var userCount = localStorage.length+1;
 
     if(username.length!=0 && useremail.length!=0){
-        localStorage.setItem('userdetails'+userCount, username+" "+useremail);
+        let obj = {
+            name : username,
+            email : useremail
+        };
+        localStorage.setItem('userdetails'+userCount, JSON.stringify(obj));
         alert('user added');
     }
     else{
@@ -29,10 +33,10 @@ document.addEventListener('DOMContentLoaded', refreshUserDetails);
 
 function refreshUserDetails(e){
     for(var i=0; i<localStorage.length; i++){
-        var userdetails = localStorage.getItem(localStorage.key(i));
+        var userdetails = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
         var li = document.createElement('li');
-        li.appendChild(document.createTextNode(userdetails));
+        li.appendChild(document.createTextNode(userdetails.name+"  "+userdetails.email));
         document.getElementById("users").appendChild(li);
 
         document.getElementById('username').value = '';
