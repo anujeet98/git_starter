20da54ae0595f0a09d2a6229344e604ad81b5106
Anujeet Swain <anujeet98@gmail.com>
2023-09-18 18:36:06 +0530

    add edit function; modified save localstorage funtion

--- bookingApp.js
+++ bookingApp.js
@@ -9,21 +9,21 @@ var deleteBtn = document.getElementsByClassName('deleteUser');
 
 form.addEventListener('submit', submitMethod);
 document.addEventListener('DOMContentLoaded', refreshUserDetails);
-userList.addEventListener('click', deleteUserDetails);
+userList.addEventListener('click', UserDetailAlter);
 
 
 
 function submitMethod(e){
     e.preventDefault();
 
-    var userCount = localStorage.length+1;
+    //var userCount = localStorage.length+1;
 
     if(username.value.length!=0 && useremail.value.length!=0){
         let obj = {
             name : username.value,
             email : useremail.value
         };
-        localStorage.setItem('userdetails'+userCount, JSON.stringify(obj));
+        localStorage.setItem(useremail.value, JSON.stringify(obj));
         alert('user added');
     }
     else{
@@ -41,14 +41,23 @@ function refreshUserDetails(e){
     for(var i=0; i<localStorage.length; i++){
         var userdetails = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
+        //create a new list element
         var li = document.createElement('li');
-        li.setAttribute('data-userKey',userdetails.name);
+        li.setAttribute('data-userKey',userdetails.email);
         li.appendChild(document.createTextNode(userdetails.name+"  "+userdetails.email));
 
+        //add delete button
         var delBtn = document.createElement('button');
         delBtn.className = "deleteUser";
         delBtn.appendChild(document.createTextNode('delete')); 
         li.appendChild(delBtn);
+
+
+        //add edit button
+        var editBtn = document.createElement('button');
+        editBtn.className = "EditUser";
+        editBtn.appendChild(document.createTextNode('edit')); 
+        li.appendChild(editBtn);
 
         userList.appendChild(li);
 
@@ -63,11 +72,26 @@ function refreshUserDetails(e){
 
 
 
-function deleteUserDetails(e){
+function UserDetailAlter(e){
     if(e.target.classList.contains('deleteUser')){
         var delUsr = e.target.parentElement;
+        
+        localStorage.removeItem(delUsr.getAttribute('data-userkey'));
         userList.removeChild(delUsr);
-        //localStorage.removeItem(delUsr.getAttribute('data-userkey'));
+    }
+
+    else if(e.target.classList.contains('EditUser')){
+        var editUsr = e.target.parentElement;
+        
+        //remove from display
+        userList.removeChild(editUsr);
+
+        var userdetails = JSON.parse(localStorage.getItem(editUsr.getAttribute('data-userKey')));
+
+        //populate input fields to enable edit
+        username.value = userdetails.name;
+        useremail.value = userdetails.email;
+        
     }
 
 
