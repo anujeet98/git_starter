3b78cfbfef12b4ef203bebca74a98d50522f118d
Anujeet Swain <anujeet98@gmail.com>
2023-09-18 16:32:15 +0530

    dynamic load user from localstorage

--- bookingApp.js
+++ bookingApp.js
@@ -12,6 +12,30 @@ function submitMethod(e){
     var useremail = document.getElementById("useremail").value;
     var userCount = localStorage.length+1;
 
-    localStorage.setItem('userdetails'+userCount, username+" "+useremail);
-    alert('user added');
+    if(username.length!=0 && useremail.length!=0){
+        localStorage.setItem('userdetails'+userCount, username+" "+useremail);
+        alert('user added');
+    }
+    else{
+        alert('Empty fields, please reenter with valid inputs');
+    }
+    window.location.reload();
+    refreshUserDetails();
+}
+
+
+document.addEventListener('DOMContentLoaded', refreshUserDetails);
+
+
+function refreshUserDetails(e){
+    for(var i=0; i<localStorage.length; i++){
+        var userdetails = localStorage.getItem(localStorage.key(i));
+
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(userdetails));
+        document.getElementById("users").appendChild(li);
+
+        document.getElementById('username').value = '';
+        document.getElementById('useremail').value = '';
+    }
 }
