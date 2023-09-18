cd564376b2e3aa844087d4e4a52c944fe0b32d2f
Anujeet Swain <anujeet98@gmail.com>
2023-09-18 18:14:38 +0530

    delete user function

--- bookingApp.js
+++ bookingApp.js
@@ -2,20 +2,26 @@
 
 var btnSubmit = document.getElementById("submitBtn");
 var form = document.getElementById("myForm");
-
+var userList = document.getElementById("users");
+var username = document.getElementById("username");
+var useremail = document.getElementById("useremail");
+var deleteBtn = document.getElementsByClassName('deleteUser');
 
 form.addEventListener('submit', submitMethod);
+document.addEventListener('DOMContentLoaded', refreshUserDetails);
+userList.addEventListener('click', deleteUserDetails);
+
+
 
 function submitMethod(e){
     e.preventDefault();
-    var username = document.getElementById("username").value;
-    var useremail = document.getElementById("useremail").value;
+
     var userCount = localStorage.length+1;
 
-    if(username.length!=0 && useremail.length!=0){
+    if(username.value.length!=0 && useremail.value.length!=0){
         let obj = {
-            name : username,
-            email : useremail
+            name : username.value,
+            email : useremail.value
         };
         localStorage.setItem('userdetails'+userCount, JSON.stringify(obj));
         alert('user added');
@@ -28,7 +34,7 @@ function submitMethod(e){
 }
 
 
-document.addEventListener('DOMContentLoaded', refreshUserDetails);
+
 
 
 function refreshUserDetails(e){
@@ -36,10 +42,33 @@ function refreshUserDetails(e){
         var userdetails = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
         var li = document.createElement('li');
+        li.setAttribute('data-userKey',userdetails.name);
         li.appendChild(document.createTextNode(userdetails.name+"  "+userdetails.email));
-        document.getElementById("users").appendChild(li);
 
+        var delBtn = document.createElement('button');
+        delBtn.className = "deleteUser";
+        delBtn.appendChild(document.createTextNode('delete')); 
+        li.appendChild(delBtn);
+
+        userList.appendChild(li);
+
+
+        //to clear the input fields after submit
         document.getElementById('username').value = '';
         document.getElementById('useremail').value = '';
     }
+}
+
+
+
+
+
+function deleteUserDetails(e){
+    if(e.target.classList.contains('deleteUser')){
+        var delUsr = e.target.parentElement;
+        userList.removeChild(delUsr);
+        //localStorage.removeItem(delUsr.getAttribute('data-userkey'));
+    }
+
+
 }
